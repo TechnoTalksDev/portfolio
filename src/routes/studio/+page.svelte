c1eb6d80c9586aef4f81b7e75f38c81a39e21cf6
TechnoTalksDev <technotalksyt101@gmail.com>
2026-04-02 20:47:33 -0700

    large studio title

--- src/routes/studio/+page.svelte
+++ src/routes/studio/+page.svelte
@@ -9,15 +9,28 @@
 ></div>
 
 <div class="w-screen h-screen bg-black">
-	<div class="h-screen w-full items-center justify-center flex">
+	<div class="h-screen w-full items-center justify-center flex flex-col">
+		<!-- <div class="relative w-auto -mb-96" transition:blur={{ amount: 5, duration: 1500 }}>
+			<h2
+				class="text-6xl gradient-heading leading-normal font-[bumbbled] whitespace-nowrap select-none"
+			>
+				chandan's
+			</h2>
+
+			<h2
+				class="text-6xl gradient-tertiary leading-normal font-[bumbbled] blur-lg absolute inset-0 whitespace-nowrap select-none"
+			>
+				chandan's
+			</h2>
+		</div> -->
 		<div class="relative w-auto" transition:blur={{ amount: 5, duration: 1500 }}>
 			<h1
-				class="text-[36cqw] gradient-hero leading-normal font-[bumbbled] whitespace-nowrap select-none"
+				class="text-[36cqw] gradient-heading leading-normal font-[bumbbled] whitespace-nowrap select-none"
 			>
 				studio
 			</h1>
 			<h1
-				class="text-[36cqw] gradient-hero leading-normal font-[bumbbled] blur-lg absolute inset-0 whitespace-nowrap select-none"
+				class="text-[36cqw] gradient-heading leading-normal font-[bumbbled] blur-lg absolute inset-0 whitespace-nowrap select-none"
 			>
 				studio
 			</h1>
